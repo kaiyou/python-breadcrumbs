790b999e360c411db25d32b928607ddc5ac70621
p.jaury <p.jaury@sysdream.com>
2013-08-14 09:38:41 +0200

    Fix a couple of typos in the setup.py script and increase the version number

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import setuptools
 
 setuptools.setup(
     name="python-breadcrumbs",
-    version="0.1.1",
+    version="0.1.2",
     author="Pierre Jaury",
     author_email="pierre@jaury.eu",
     description="Store breadcrumbs for later accessing an object internals",
     long_description=open('README.txt').read(),
-    license="MIT",
+    license="GPLv3",
     url="https://github.com/kaiyou/python-breadcrumbs",
-    py_modules=['breadcrumbs'],
+    py_modules=["breadcrumbs"],
     test_suite="tests"
 )
